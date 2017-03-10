149fb44e7566b561e9cfa62c4b3a21d6db1b82a9
Alexandr Marchenko <marchenko.alexandr@gmail.com>
2017-03-10 16:37:57 +0200

    initial

---
+++ AzTable/Metrics.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Table;
+
+namespace AzTable
+{
+	public class Metrics
+	{
+		protected readonly CloudTable Table;
+
+		public Metrics(string accountName, string accountKey)
+		{
+			Table = CloudStorageAccount
+				.Parse($"DefaultEndpointsProtocol=https;AccountName={accountName};AccountKey={accountKey}")
+				.CreateCloudTableClient()
+				.GetTableReference("metrics");
+
+			Table.CreateIfNotExists();
+		}
+
+		public TableResult Save(string metric, double value, DateTime date) => Table.Execute(TableOperation.InsertOrReplace(new Metric
+		{
+			PartitionKey = metric,
+			RowKey = new DateTimeOffset(date).ToUnixTimeSeconds().ToString()
+		}));
+
+		internal class Metric : TableEntity
+		{
+			public double Value { get; set; }
+		}
+	}
+}
